854d85698a4590a927a3ee7e1c5ad032d3609f29
1301yashraj <1301yashraj@gmail.com>
2022-09-29 00:51:23 +0530

    Connection Made

--- Automated Auction System/src/raj/yash/Connection/ConnectTODB.java
+++ Automated Auction System/src/raj/yash/Connection/ConnectTODB.java
@@ -1,5 +1,35 @@
 package raj.yash.Connection;
 
-public class ConnectTODB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectTODB {
+    
+	public static Connection connect() {
+		Connection con = null;
+		//load Driver in memory 
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		//Making a Connection with Our Database 
+		String url = "jdbc:mysql://localhost:3306/Automated_Auction";
+		try
+		{
+		 con = DriverManager.getConnection(url,"root","root");
+		} 
+		catch (SQLException e)
+		{
+		 e.printStackTrace();
+		}
+			
+
+		return con;
+	}
 }
